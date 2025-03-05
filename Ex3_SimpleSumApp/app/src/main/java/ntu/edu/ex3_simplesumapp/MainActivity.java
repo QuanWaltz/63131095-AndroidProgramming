8fa7642d4b5a73bc14c37121c1caebe7dc9c6394
Huỳnh Đỗ Anh Quân <waltz18092003@gmail.com>
2025-03-05 23:46:11 +0700

    Done
    Done activity_main.xml
    Done MainActivity.java

--- Ex3_SimpleSumApp/app/src/main/java/ntu/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/ntu/edu/ex3_simplesumapp/MainActivity.java
@@ -7,18 +7,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void doSum(View view){
+        EditText editNumA = findViewById(R.id.editNumA);
+        EditText editNumB = findViewById(R.id.editNumB);
+        EditText editSum = findViewById(R.id.editSum);
+
+        double a = Double.parseDouble(editNumA.getText().toString());
+        double b = Double.parseDouble(editNumB.getText().toString());
+
+        double tong = a + b;
+
+        editSum.setText(String.valueOf(tong));
+
     }
 }
