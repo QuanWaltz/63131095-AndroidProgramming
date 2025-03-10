32cda5a2ba2221f0ebb5724914b3dac326c8a3a6
Huỳnh Đỗ Anh Quân <waltz18092003@gmail.com>
2025-03-10 21:23:45 +0700

    Update MainActivity.java
    declare

--- Ex4_AddSubMulDiv_OnClick/app/src/main/java/ntu/edu/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_OnClick/app/src/main/java/ntu/edu/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -7,9 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editA, editB, editResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void calcSum(View v){
+        double a = Double.parseDouble(editA.getText().toString());
+        double b = Double.parseDouble(editB.getText().toString());
+        double add = a + b;
+        editResult.setText(String.valueOf(add));
+    }
+    public void calcSub(View v){
+        double a = Double.parseDouble(editA.getText().toString());
+        double b = Double.parseDouble(editB.getText().toString());
+        double diff = a - b;
+        editResult.setText(String.valueOf(diff));
+    }
+    public void calcMul(View v){
+        double a = Double.parseDouble(editA.getText().toString());
+        double b = Double.parseDouble(editB.getText().toString());
+        double prod = a * b;
+        editResult.setText(String.valueOf(prod));
+    }
+    public void calcDiv(View v){
+        double a = Double.parseDouble(editA.getText().toString());
+        double b = Double.parseDouble(editB.getText().toString());
+        double quotient = a / b;
+        editResult.setText(String.valueOf(quotient));
     }
 }
