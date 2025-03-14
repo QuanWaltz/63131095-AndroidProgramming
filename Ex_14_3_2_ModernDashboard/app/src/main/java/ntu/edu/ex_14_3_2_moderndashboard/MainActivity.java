81383abfb49b57d6e98dcc4ad278eacdbf7bdfbd
Huỳnh Đỗ Anh Quân <waltz18092003@gmail.com>
2025-03-14 17:36:35 +0700

    Done
    Add new activity name clock
    intent to clock

--- Ex_14_3_2_ModernDashboard/app/src/main/java/ntu/edu/ex_14_3_2_moderndashboard/MainActivity.java
+++ Ex_14_3_2_ModernDashboard/app/src/main/java/ntu/edu/ex_14_3_2_moderndashboard/MainActivity.java
@@ -1,24 +1,25 @@
 package ntu.edu.ex_14_3_2_moderndashboard;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView clockCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        clockCard = findViewById(R.id.clockCard);
+        clockCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClockActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
